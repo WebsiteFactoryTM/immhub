b33b38a33e9afaf8fddda8067eeda5ff06ffc133
Alex Nedelia <alex.nedelia@gmail.com>
2024-02-29 13:27:38 +0200

    lets do it!

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,12 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <div className="flex flex-col w-screen text-center h-[100vh] justify-center align-middle bg-gradient-to-b from-purple-900 to-slate-50">
+      <h2 className="text-white pt-10  text-2xl">Pixel Factory presents:</h2>
+      <h1 className="text-purple-900  pt-10 text-4xl font-bold align-top underline">
+        ImmHub
+      </h1>
+    </div>
+  );
 }
